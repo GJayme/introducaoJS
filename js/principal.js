23ee6a32f77c510aa41b3acca3a574756a3542f9
Gabriel Jayme <gabrielj_89@hotmail.com>
2019-12-16 14:29:58 -0300

    Calculando IMC de todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -5,39 +5,45 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
 
 
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
 
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var pesoValida = true;
+    var alturaValida = true;
 
 
-var pesoValida = true;
-var alturaValida = true;
+    if (peso <= 0 || peso >= 1000){
+        console.log("Peso invalalido!");
+        tdImc.textContent = "Peso Inválido!";
+        pesoValida = false;
+    }
 
 
-if (peso <= 0 || peso >= 1000){
-    console.log("Peso invalalido!");
-    tdImc.textContent = "Peso Inválido!";
-    pesoValida = false;
+    if (altura <= 0 || altura >= 3){
+        console.log("Altura invalalido!");
+        tdImc.textContent = "Altura Inválido!";
+        alturaValida = false;
+    }
+
+
+    if (pesoValida && alturaValida){
+        var imc = peso / (altura*altura);
+        tdImc.textContent = imc.toFixed(2);
+    } else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
 }
 
 
-if (altura <= 0 || altura >= 3){
-    console.log("Altura invalalido!");
-    tdImc.textContent = "Altura Inválido!";
-    alturaValida = false;
-}
-
-
-if (pesoValida && alturaValida){
-    var imc = peso / (altura*altura);
-    tdImc.textContent = imc;
-} else {
-    tdImc.textContent = "Altura e/ou peso inválidos!"
-}
