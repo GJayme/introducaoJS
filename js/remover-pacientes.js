c7d7be3507dc044bbe520140a261f51b7a2f7168
Gabriel Jayme <gabrielj_89@hotmail.com>
2019-12-20 12:45:45 -0300

    Refatoração da funcção remover paciente, utilizando Event.Target e parentNode.

--- js/remover-pacientes.js
+++ js/remover-pacientes.js
@@ -1,8 +1,8 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.addEventListener("dblclick", function(event){
+    
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; // TR = Paciente = remover
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){ // Adiciona o elemento de escutar a interação do usuário
-        console.log("Fui clicado com um duplo click;");
-        this.remove(); // palavra de contexto, refere-se ao dono do evento. Nesse caso, quem foi clicado.
-    }); 
+    paiDoAlvo.remove();
 });
