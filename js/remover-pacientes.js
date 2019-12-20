0202f410c88c5f7f2ae19048846c0247635bb061
Gabriel Jayme <gabrielj_89@hotmail.com>
2019-12-20 13:45:19 -0300

    Função remover paciente com efeito CSS

--- js/remover-pacientes.js
+++ js/remover-pacientes.js
@@ -1,8 +1,9 @@
 var tabela = document.querySelector("#tabela-pacientes");
 tabela.addEventListener("dblclick", function(event){
     
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode; // TR = Paciente = remover
-
-    paiDoAlvo.remove();
+    event.target.parentNode.classList.add("fadeOut");
+    
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
 });
